17e5de1ecf5a6505cdbbf7490c83690fe4dd2ee6
David Ladowitz <david@ladowitz.com>
2018-01-21 17:47:12 -0800

    end to end test

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -36,6 +36,61 @@ describe('Lottery Contract', () => {
 
     const players = await lottery.methods.getPlayers().call({ from: accounts[0] });
 
+    assert.equal(1, players.length);
     assert.equal(accounts[0], players[0]);
   });
+
+  it('allows two addresses to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('1', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('2', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({ from: accounts[0]})
+    assert.equal(2, players.length);
+    assert.equal(accounts[1], players[1]);
+  })
+
+  it('requires a mininum amount of ether to run', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value:  web3.utils.toWei('0.000001', 'ether')
+      });
+
+      assert(false); //this just falls into catch with err.
+    } catch (err) {
+      assert(err);
+      assert.notEqual('AssertionError [ERR_ASSERTION]', err['name']);
+    }
+  });
+
+  it('only allows the manager to call pickWinner', async () => {
+    try {
+      await lottery.methods.enter().send({ from: accounts[1], value:  web3.utils.toWei('1', 'ether') });
+      await lottery.methods.pickWinner().call({ from: accounts[1] });
+      assert(false); //this just falls into catch with err.
+    } catch (err) {
+      assert(err);
+      assert.notEqual('AssertionError [ERR_ASSERTION]', err['name']);
+    }
+  });
+
+  it('sends money to the winner and resets the players array', async () => {
+    let players;
+
+    await lottery.methods.enter().send({ from: accounts[1], value:  web3.utils.toWei('1', 'ether') });
+    players = await lottery.methods.getPlayers().call({ from: accounts[0] });
+    assert.equal(1, players.length);
+
+    await lottery.methods.pickWinner().send({ from: accounts[0] })
+    players = await lottery.methods.getPlayers().call({ from: accounts[0] });
+    assert.equal(0, players.length);
+  })
+
 });
